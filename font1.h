e04ae288915b23908103744a7495ecde26930b94
Quantum Club <41433080+quantumclub@users.noreply.github.com>
2019-04-03 23:00:28 +0200

    Add files via upload

--- font1.h
+++ font1.h
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   font1.h
- * Author: root
+ * Author: Hennie Kotze info@autonomech.co.za
  *
  * Created on 03 April 2019, 2:38 PM
  */
